32f3a2b40f3e2fb26578d8aaedcbd81513b59375
Leonardo Peixoto <leonardo.lima@saudeid.com.br>
2022-07-15 16:56:20 -0300

    feat: add config of application

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+import { OpenWeatherConfig } from "@weather-party/config/open-weather.config";
+
+export const configs = [OpenWeatherConfig];
---
+++ src/config/open-weather.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+export const OpenWeatherConfig = registerAs('open-weather', () => ({
+  baseUrl: process.env.OPEN_WEATHER_BASE_URL,
+  auth: {
+    apiKey: process.env.OPEN_WEATHER_API_KEY,
+  },
+  endpoints: {
+    getCurrentTemperature: process.env.OPEN_WEATHER_GET_CURRENT_TEMPERATURE_ENDPOINT,
+  },
+}));
